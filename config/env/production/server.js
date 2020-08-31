316e7c4def8145fb126b95fae0f64df91face778
nrd89 <ndaltondev@gmail.com>
2020-08-30 21:46:16 -0700

    added: secret env variable to server.js file

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "127.0.0.1"),
-  port: env.int("PORT", 1337), 
-  url: "https://api.rouse.yoga", 
+  port: env.int("PORT", 1337),
+  url: "https://api.rouse.yoga",
   admin: {
-      url: "https://admin.rouse.yoga", 
-      serveAdminPanel: false,
+    url: "https://admin.rouse.yoga",
+    serveAdminPanel: false,
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
   },
 });
